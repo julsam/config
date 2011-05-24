a66287dc766cd7a22c84a73406eff8428c693b9f
Philippe Pepiot <phil@philpep.org>
2011-05-24 21:44:09 +0200

    Add install script

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -e
+
+FILES="gitconfig inputrc screenrc tmux.conf vim vimrc zshrc"
+BASEPATH=$(pwd)
+
+if [ -z "${HOME}" ]
+then
+	echo "[!] need \$HOME set"
+	exit 1
+fi
+
+for file in $FILES
+do
+	src="${BASEPATH}/${file}"
+	dst="${HOME}/.${file}"
+
+	if [ -e "${dst}" ]
+	then
+		echo "[!] ${dst} already exist, skip"
+	else
+		echo ln -s '"'${src}'"' '"'${dst}'" ? [y/n] '
+		read ans
+		if [ "${ans}" = "y" ]
+		then
+			ln -s "${src}" "${dst}"
+		fi
+	fi
+done
